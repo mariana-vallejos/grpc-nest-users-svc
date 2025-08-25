bebba89636ae721362cafe543c8c13fb7221ccd4
Mariana Vallejos <mariana.vallejos@assuresoft.tech>
2025-08-25 09:13:43 -0700

    Add: user entity and db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dbConfig from './config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const db = configService.get('database');
+        return {
+          type: 'postgres',
+          host: db.host,
+          port: db.port,
+          username: db.username,
+          password: db.password,
+          database: db.database,
+          synchronize: db.synchronize,
+          autoLoadEntities: true,
+        };
+      },
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from "typeorm"
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    username: string
+
+    @Column()
+    email: string
+
+    @CreateDateColumn()
+    created_at: Date
+}
